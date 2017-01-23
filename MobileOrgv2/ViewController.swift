5d0de01d530d85609aef08d05a6b26612c94ff03
Mario Martelli <cocoa@schnuddelhuddel.de>
2017-01-23 10:12:15 +0100

    Initial Commit

---
+++ MobileOrgv2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MobileOrgv2
+//
+//  Created by Mario Martelli on 23.01.17.
+//  Copyright © 2017 MobileOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
